950fc70ac4a45f982426936354b7d133e10259ee
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2020-05-22 16:38:33 -0300

    initial commit

---
+++ src/test/java/br/com/mvj/buckpal/BuckpalAppTests.java
@@ -0,0 +1,13 @@
+package br.com.mvj.buckpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuckpalAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
