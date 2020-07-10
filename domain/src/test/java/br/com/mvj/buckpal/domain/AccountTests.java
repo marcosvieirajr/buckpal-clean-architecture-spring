92522fa4940a48af542b2889717a5cb947f4d068
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2020-07-10 09:00:22 -0300

    update file

--- domain/src/test/java/br/com/mvj/buckpal/domain/AccountTests.java
+++ domain/src/test/java/br/com/mvj/buckpal/domain/AccountTests.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AccountTest {
+public class AccountTests {
 
     @Test
     void dopositSucceeds() {
